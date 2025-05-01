9498e3d8967482490d73ac65df5b730d158c1928
Alexey Stupnikov <aleks.gugl.77@gmail.com>
2025-05-01 17:24:11 +0400

    верстка страницы с сообщением

--- app/message/page.tsx
+++ app/message/page.tsx
@@ -1,10 +1,7 @@
 import { css } from "@/styled-system/css";
-import { Footer } from "../components/footer";
-import { Header } from "../components/header";
 import { container } from "../recipes/container";
 import { grayBox } from "../recipes/gray-box";
 import { header } from "../recipes/header";
-import { Form } from "../components/form";
 
 const contactUs = css({
     padding: '100px 0'
@@ -15,8 +12,7 @@ export default function Page() {
         <div className={grayBox()}>
             <div className={container()}>
                 <div className={contactUs}>
-                    <h1 className={`${header({ visual: 'center' })} ${css({ mb: 3 })}`}>Only CTA on the page</h1>
-                    <Form />
+                    <h1 className={header({ visual: 'center' })}>Message generated on the server</h1>
                 </div>
             </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
-import { Header } from "./components/header";
 import { Info } from "./components/info";
 import { Intro } from "./components/intro";
 import { Call } from "./components/call";
-import { Footer } from "./components/footer";
 
 export default function Home() {
     return (
         <>
-            <Header/>
             <Intro/>
             <Info/>
             <Call/>
-            <Footer/>
         </>
     )
 }
